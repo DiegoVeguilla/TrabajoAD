f86f38d1097641bf282e509a166b6bd970698403
Valero004 <ana.valero@cesjuanpablosegundo.es>
2021-11-02 16:25:40 +0100

    Update(JSON)

--- Doctor_Strange/src/main/java/com/example/doctor_strange/Personaje.java
+++ Doctor_Strange/src/main/java/com/example/doctor_strange/Personaje.java
@@ -4,70 +4,41 @@ import java.util.ArrayList;
 
 public class Personaje {
     //atributos
-    int identificador, coste, recompensa, inteligencia, fuerza, velocidad, resistencia, habLucha, proyecEnergia;
+    int identificador, coste, recompensa;
+    ArrayList<Integer> Poderes = new ArrayList();//, inteligencia, fuerza, velocidad, resistencia, habLucha, proyecEnergia
     String origen, tipo;
     String alias, identidadSecreta, organizacion;
     ArrayList<Movimiento> listaMovimientos = null;
     double energiaVital, energiaLucha;
-
     //constructor
-    public Personaje(){}
+
+    public Personaje() {}
+
     //metodos
 
     //getters && setters
     public int getIdentificador() {
         return identificador;
     }
+
     public void setIdentificador(int identificador) {
         this.identificador = identificador;
     }
+
     public int getCoste() {
         return coste;
     }
+
     public void setCoste(int coste) {
         this.coste = coste;
     }
+
     public int getRecompensa() {
         return recompensa;
     }
+
     public void setRecompensa(int recompensa) {
         this.recompensa = recompensa;
-    }
-    public int getInteligencia() {
-        return inteligencia;
-    }
-    public void setInteligencia(int inteligencia) {
-        this.inteligencia = inteligencia;
-    }
-    public int getFuerza() {
-        return fuerza;
-    }
-    public void setFuerza(int fuerza) {
-        this.fuerza = fuerza;
-    }
-    public int getVelocidad() {
-        return velocidad;
-    }
-    public void setVelocidad(int velocidad) {
-        this.velocidad = velocidad;
-    }
-    public int getResistencia() {
-        return resistencia;
-    }
-    public void setResistencia(int resistencia) {
-        this.resistencia = resistencia;
-    }
-    public int getHabLucha() {
-        return habLucha;
-    }
-    public void setHabLucha(int habLucha) {
-        this.habLucha = habLucha;
-    }
-    public int getProyecEnergia() {
-        return proyecEnergia;
-    }
-    public void setProyecEnergia(int proyecEnergia) {
-        this.proyecEnergia = proyecEnergia;
     }
 
     public String getOrigen() {
@@ -81,34 +52,41 @@ public class Personaje {
     public String getAlias() {
         return alias;
     }
+
     public void setAlias(String alias) {
         this.alias = alias;
     }
+
     public String getIdentidadSecreta() {
         return identidadSecreta;
     }
+
     public void setIdentidadSecreta(String identidadSecreta) {
         this.identidadSecreta = identidadSecreta;
     }
+
     public String getOrganizacion() {
         return organizacion;
     }
+
     public void setOrganizacion(String organizacion) {
         this.organizacion = organizacion;
     }
+
     public double getEnergiaVital() {
         return energiaVital;
     }
+
     public void setEnergiaVital(double energiaVital) {
         this.energiaVital = energiaVital;
     }
+
     public double getEnergiaLucha() {
         return energiaLucha;
     }
+
     public void setEnergiaLucha(double energiaLucha) {
         this.energiaLucha = energiaLucha;
     }
 
-    //peronsajes
-    Personaje doctorStrange = new Personaje();
 }
