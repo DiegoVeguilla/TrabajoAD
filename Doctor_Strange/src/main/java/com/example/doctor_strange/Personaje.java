a874b574b4605fe3736c5440cf00398022399a00
Valero004 <ana.valero@cesjuanpablosegundo.es>
2021-11-02 18:56:36 +0100

    Update Personaje.java

--- Doctor_Strange/src/main/java/com/example/doctor_strange/Personaje.java
+++ Doctor_Strange/src/main/java/com/example/doctor_strange/Personaje.java
@@ -16,75 +16,58 @@ public class Personaje {
 
     //metodos
 
-    //getters && setters
+    //getters && setter
     public int getIdentificador() {
         return identificador;
     }
-
     public void setIdentificador(int identificador) {
         this.identificador = identificador;
     }
-
     public int getCoste() {
         return coste;
     }
-
     public void setCoste(int coste) {
         this.coste = coste;
     }
-
     public int getRecompensa() {
         return recompensa;
     }
-
     public void setRecompensa(int recompensa) {
         this.recompensa = recompensa;
     }
-
     public String getOrigen() {
         return origen;
     }
-
     public void setOrigen(String origen) {
         this.origen = origen;
     }
-
     public String getAlias() {
         return alias;
     }
-
     public void setAlias(String alias) {
         this.alias = alias;
     }
-
     public String getIdentidadSecreta() {
         return identidadSecreta;
     }
-
     public void setIdentidadSecreta(String identidadSecreta) {
         this.identidadSecreta = identidadSecreta;
     }
-
     public String getOrganizacion() {
         return organizacion;
     }
-
     public void setOrganizacion(String organizacion) {
         this.organizacion = organizacion;
     }
-
     public double getEnergiaVital() {
         return energiaVital;
     }
-
     public void setEnergiaVital(double energiaVital) {
         this.energiaVital = energiaVital;
     }
-
     public double getEnergiaLucha() {
         return energiaLucha;
     }
-
     public void setEnergiaLucha(double energiaLucha) {
         this.energiaLucha = energiaLucha;
     }
