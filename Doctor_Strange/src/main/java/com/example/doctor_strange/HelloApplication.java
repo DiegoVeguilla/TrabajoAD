75c1d200685b8849d249676f89b2dda68a339a4e
Mario-Munoz-Barroso <72133132+Mario-Munoz-Barroso@users.noreply.github.com>
2021-11-10 19:32:44 +0100

    asd

--- Doctor_Strange/src/main/java/com/example/doctor_strange/HelloApplication.java
+++ Doctor_Strange/src/main/java/com/example/doctor_strange/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu_view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
     }
 
---
+++ Doctor_Strange/src/main/java/com/example/doctor_strange/MenuController.java
@@ -0,0 +1,4 @@
+package com.example.doctor_strange;
+
+public class MenuController {
+}
