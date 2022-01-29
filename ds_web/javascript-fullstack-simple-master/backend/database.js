b7fdb0f85c52e12b091d082753a41f527418e3a1
Laura-30 <72133179+Laura-30@users.noreply.github.com>
2022-01-29 12:33:55 +0100

    update

--- ds_web/javascript-fullstack-simple-master/backend/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true
-}) 
-    .then(db => console.log(`DB is connected`))
-    .catch(err => console.error(err));
