29c1975d848e36cef6d255e5f40accf15f8ef75a
Valero004 <ana.valero@cesjuanpablosegundo.es>
2022-02-13 22:04:56 +0100

    update

--- Doctor_Strange/src/main/java/com/example/doctor_strange/JugarPartida.java
+++
@@ -1,8 +0,0 @@
-package com.example.doctor_strange;
-
-public class JugarPartida {
-    public static void main(String[] args) {
-        Partida partida = new Partida();
-        partida.jugarPartida();
-    }
-}
